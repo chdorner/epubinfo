203629883dab284fa692b153c4f4959aac09ee77
Mehmet Celik <mehmet@celik.be>
2013-10-23 16:03:56 +0200

    some refactoring

--- lib/epubinfo/models/table_of_contents.rb
+++ lib/epubinfo/models/table_of_contents.rb
@@ -1,3 +1,5 @@
+require 'epubinfo/models/table_of_contents/resource'
+
 module EPUBInfo
   module Models
     class TableOfContents
@@ -19,35 +21,7 @@ module EPUBInfo
       end
 
       def resources
-        @resources ||=
-            begin
-              resources = []
-              spine.first.xpath('./itemref').map {|s| s['idref']}.each do |item_ref|
-                resource = manifest.css("##{item_ref}").first
-                if resource
-                  uri = resource.attr('href')
-                  mime_type = resource.attr('media-type')
-                  label = ''
-                  uri_ref = ''
-                  order = ''
-
-                  nav_point = document.xpath("//navPoint[starts-with(content/@src,'#{uri}')]").first
-                  if nav_point
-                    label = nav_point.at('navLabel text').content || ''
-                    uri_ref = nav_point.at('content').attr('src') || ''
-                    order   = nav_point.attr('playOrder') || ''
-                  end
-
-                  resources << {:id      => item_ref,
-                                :uri     => parser.zip_file.entries.map {|p| p.name}.select{|s| s.match(uri)}.first,
-                                :uri_ref => uri_ref,
-                                :text    => label,
-                                :type    => mime_type,
-                                :order   => order}
-                end
-              end
-              resources
-            end
+        @resources ||= Resource.new(self)
       end
 
       def document
@@ -58,16 +32,16 @@ module EPUBInfo
         @toc_path ||= begin
           toc_id = spine[0]['toc']
           toc_ncx = manifest.xpath("item[@id = '#{toc_id}']").first.attr('href')
-          parser.zip_file.entries.map {|p| p.name}.select{|s| s.match(toc_ncx)}.first
+          parser.zip_file.entries.map { |p| p.name }.select { |s| s.match(toc_ncx) }.first
         end
 
       end
 
-      private
       attr_accessor :manifest
       attr_accessor :parser
       attr_accessor :spine
 
+      private
       def load_toc_file
         Nokogiri::XML(parser.zip_file.read(path))
       end
